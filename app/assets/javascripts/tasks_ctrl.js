da749be28615c44f33e63b9c218f41d186fcc061
JackMarx <seriousfools@gmail.com>
2017-02-26 11:34:38 -0600

    initial commit

---
+++ app/assets/javascripts/tasks_ctrl.js
@@ -0,0 +1,27 @@
+(function() {
+  "use strict";
+
+  angular.module("app").controller("tasksCtrl", function($scope) {
+    
+    $scope.tasks = [
+                    "Mow the lawn", 
+                    "Take out trash", 
+                    "Feed the cat"
+                    ];
+
+    $scope.addTask = function(task) {
+      if(task){
+        $scope.tasks.push(task);
+        $scope.newTask = null;
+      }
+    };
+
+    $scope.completeTask = function(index) {
+      // var index = $scope.tasks.indexOf(task);
+      $scope.tasks.splice(index,1);
+    };
+
+    window.scope = $scope;
+  });
+
+}());
